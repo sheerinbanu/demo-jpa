622025794d895405413e7659fd0f9dcf54ae320e
Sheerin Banu <sheemuzz@outlook.com>
2024-11-06 11:31:49 +0100

    exo2 TP1

---
+++ src/main/java/fr/diginamic/xml/TestConfigurationXML.java
@@ -0,0 +1,39 @@
+package fr.diginamic.xml;
+
+import org.apache.commons.configuration2.XMLConfiguration;
+import org.apache.commons.configuration2.builder.fluent.Configurations;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class TestConfigurationXML {
+    public static void main(String[] args) {
+
+        Configurations configs = new Configurations();
+
+        try {
+            XMLConfiguration config = configs.xml("fichier.xml");
+            String nomHost = config.getString("database.host");
+            String nomBase = config.getString("database[@nom]");
+            System.out.println(nomBase);
+            System.out.println(nomHost);
+        } catch (ConfigurationException e) {
+            throw new RuntimeException(e);
+        }
+
+        Path pathfile = Paths.get("src\\main\\resources\\fichier.xml");
+        try {
+            List<String> lines = Files.readAllLines(pathfile, StandardCharsets.UTF_8);
+            for (String line : lines) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
